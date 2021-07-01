5bb7704f5c18b66c409f722c3d239f5a2b6f7500
NoahAlbert <no.albert113@gmail.com>
2021-07-01 15:12:51 -0400

    uncommented code

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,26 +1,26 @@
-// const fruits = [
-//     'banana',
-//     'orange',
-//     'apple'
-// ]
+const fruits = [
+    'banana',
+    'orange',
+    'apple'
+]
 
-// // console.log('should get orange:' , fruits[1])
-// // console.log('the array is this length:' , fruits.length)
-// fruits.push('lemon')
-// fruits.shift()
-// fruits.push({newFruit: '🍎'})
-// fruits.unshift ({newFruit: 'sunflower'})
-// console.log('fruits:',fruits)
+console.log('should get orange:' , fruits[1])
+console.log('the array is this length:' , fruits.length)
+fruits.push('lemon')
+fruits.shift()
+fruits.push({newFruit: '🍎'})
+fruits.unshift ({newFruit: 'sunflower'})
+console.log('fruits:',fruits)
 
-// const shoppingList = ['shampoo' , 'soap' , 'milk' , '🥑']
+const shoppingList = ['shampoo' , 'soap' , 'milk' , '🥑']
 
-// const newShoppingList = shoppingList.concat(fruits)
-// console.log('============')
-// console.log('newShoppingList:',newShoppingList.toString())
+const newShoppingList = shoppingList.concat(fruits)
+console.log('============')
+console.log('newShoppingList:',newShoppingList.toString())
 
-// const cars = ['ford', 'audi', 'bmw', 'vw', 'nissan', 'mazda', 'honda', 'hyundai', 'bus']
-// cars.push(...['toyota', 'mitsubishi'])
-// console.log(cars)
+const cars = ['ford', 'audi', 'bmw', 'vw', 'nissan', 'mazda', 'honda', 'hyundai', 'bus']
+cars.push(...['toyota', 'mitsubishi'])
+console.log(cars)
 
 const str = 'jello pancakes'
 console.log(str.length)
